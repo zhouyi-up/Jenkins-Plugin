207806eab2a8f888e46360075fea5ff986e896b1
LiuJun <liujun@liu-jun.top>
2020-12-18 22:23:56 +0800

    2020.2

--- src/main/java/com/jenkins/compent/JenkinsNotificationComponent.java
+++ src/main/java/com/jenkins/compent/JenkinsNotificationComponent.java
@@ -1,10 +1,13 @@
 package com.jenkins.compent;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.notification.Notification.CollapseActionsDirection;
+import javax.swing.Icon;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.notification.NotificationListener;
 
 import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationGroupManager;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
 
 /**
  * @author liujun
@@ -14,28 +17,28 @@ public class JenkinsNotificationComponent {
     public static final String JENKINS_GROUP = "Jenkins Group";
 
     public static void notifyError(Project project, String content) {
-        NotificationGroupManager.getInstance().getNotificationGroup(JENKINS_GROUP)
-                .createNotification(content, NotificationType.ERROR)
-                .notify(project);
+        Notification notification = new Notification(JENKINS_GROUP, "",content, NotificationType.ERROR);
+        notification.setContent(content);
+        notification.notify(project);
     }
 
     public static void notifyError(Project project,String title, String content) {
-        Notification notification = NotificationGroupManager.getInstance().getNotificationGroup(JENKINS_GROUP)
-                .createNotification(content, NotificationType.ERROR);
+        Notification notification = new Notification(JENKINS_GROUP, "",content, NotificationType.ERROR);
+        notification.setContent(content);
         notification.setTitle(title);
         notification.notify(project);
     }
 
     public static void notifySuccess(Project project, String content){
-        NotificationGroupManager.getInstance().getNotificationGroup(JENKINS_GROUP)
-                .createNotification(content, NotificationType.INFORMATION)
-                .notify(project);
+        Notification notification = new Notification(JENKINS_GROUP, "",content, NotificationType.INFORMATION);
+        notification.setContent(content);
+        notification.notify(project);
     }
 
     public static void notifyWarning(Project project, String content){
-        NotificationGroupManager.getInstance().getNotificationGroup(JENKINS_GROUP)
-                .createNotification(content, NotificationType.WARNING)
-                .notify(project);
+        Notification notification = new Notification(JENKINS_GROUP, "",content, NotificationType.WARNING);
+        notification.setContent(content);
+        notification.notify(project);
     }
 
 }
